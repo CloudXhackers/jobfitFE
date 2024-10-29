03ce5fcad1d33cc6a98a1ea0fa1d87dc6cd27ac4
PraneethChitturi <praneethchitturi12@gmail.com>
2024-10-28 18:47:59 -0700

    ThirdSectionofHeroPage

--- src/components/Public/heroPage.tsx
+++ src/components/Public/heroPage.tsx
@@ -1,10 +1,48 @@
 import Container from "@mui/material/Container"
-import { Box, IconButton, Typography } from "@mui/material"
+import { Box, Grid, Icon, IconButton, Link, Typography } from "@mui/material"
 import { BaseButton, StartButton } from "../../common/buttonStyles"
 import LeftImage from "../../images/BgHomePage.jpeg"
 import RightImage from "../../images/ai-research-scientist-abstract-concept-600nw-2368588939.webp"
 import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline"
 import TypeImg from "../../images/m_image.png"
+import JobSearchIcon from "../../images/container-md (1).png"
+import ResumeIcon from "../../images/container-md (2).png"
+import LockIcon from "../../images/container-md (3).png"
+import LoveIcon from "../../images/container-md (4).png"
+
+const features = [
+  {
+    icon: JobSearchIcon,
+    title: "Job Search",
+    description:
+      "Discover new opportunities tailored to your skills and  preferences.",
+    linkText: "Explore Job Opportunities",
+    linkUrl: "/job-search",
+  },
+  {
+    icon: ResumeIcon,
+    title: "Resume & Cover Letter Builder",
+    description:
+      "Craft a standout resume with our easy-to-use modification tools.",
+    linkText: "Build Your Resume",
+    linkUrl: "/resume-builder",
+  },
+  {
+    icon: LockIcon,
+    title: "AutoBot Assist",
+    description: "Use AI Bot to go FSD on application streak.",
+    linkText: "Get Cover Letter Help",
+    linkUrl: "/cover-letter-help",
+  },
+  {
+    icon: LoveIcon,
+    title: "Application Tracker",
+    description:
+      "Effortlessly manage and track your job applications in one place.",
+    linkText: "Manage Applications Easily",
+    linkUrl: "/application-tracker",
+  },
+]
 
 export default function HeroPage() {
   return (
@@ -143,6 +181,78 @@ export default function HeroPage() {
           src={TypeImg}
           alt="Bottom Right"
         />
+      </Container>
+      <Typography variant="h6" sx={{ fontWeight: "750", paddingTop: 6 }}>
+        Automate Your Job Hunt
+      </Typography>
+      <Typography
+        variant="h4"
+        sx={{ fontWeight: "750", p: 1, paddingBottom: 8 }}
+      >
+        Unlock Your Career Potential Today
+      </Typography>
+      <Container class="h-[650px]  justify-center text-center items-center ">
+        <Grid
+          container
+          spacing={3}
+          sx={{
+            maxWidth: 1200,
+            height: 500,
+            margin: "auto",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {features.map((feature, index) => (
+            <Grid item xs={6} sm={6} key={index}>
+              <Box
+                sx={{
+                  border: "1px solid #ddd",
+                  borderRadius: 4,
+                  padding: 4,
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "flex-start",
+                  height: "100%",
+                  bgcolor: "white",
+                }}
+              >
+                <Box
+                  component="img"
+                  src={feature.icon}
+                  alt={feature.title}
+                  sx={{ width: 45, height: 45, mb: 4 }}
+                />
+                <Box>
+                  <Typography
+                    variant="h6"
+                    fontWeight="bold"
+                    className="flex pl-4 pb-2"
+                    color="text.primary"
+                  >
+                    {feature.title}
+                  </Typography>
+                  <Typography
+                    variant="h7"
+                    color="text.secondary"
+                    className="flex pl-4 text-start"
+                    mb={4}
+                  >
+                    {feature.description}
+                  </Typography>
+                  <Link
+                    href={feature.linkUrl}
+                    variant="body2"
+                    color="primary"
+                    class="flex pl-4 font-bold text-blue-600"
+                  >
+                    {feature.linkText}
+                  </Link>
+                </Box>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
       </Container>
     </>
   )
