dbfcb4d812e275e099125c4cb865b62953f9273a
Danielxburns <danielburnstech@gmail.com>
2024-10-31 20:10:35 -0500

    add 'Status' drop down menu

--- src/components/User/Applications/applicationsTable.tsx
+++ src/components/User/Applications/applicationsTable.tsx
@@ -1,6 +1,13 @@
-import { Box } from "@mui/material"
-import type { GridRowsProp, GridColDef } from "@mui/x-data-grid"
-import { DataGrid } from "@mui/x-data-grid"
+import type { SelectChangeEvent } from "@mui/material";
+import { Box, FormControl, MenuItem, Select } from "@mui/material";
+import type {
+  GridColDef,
+  GridRenderCellParams,
+  GridRowsProp,
+} from "@mui/x-data-grid";
+import { DataGrid } from "@mui/x-data-grid";
+import { useState } from "react";
+
 
 const columns: GridColDef[] = [
   {
@@ -57,8 +64,50 @@ const columns: GridColDef[] = [
     headerAlign: "center",
     headerClassName: "text-white font-semi-bold text-sm",
     align: "center",
-  },
+    renderCell: StatusMenu
+  }
 ]
+
+function StatusMenu(params: GridRenderCellParams<String>) {
+
+  const [value, setValue] = useState(params.value)
+
+type MenuItemColor = {
+  applied: string;
+  waiting: string;
+  inteviewing: string;
+  offer: string;
+  declined: string;
+}
+
+  const menuItemColor: MenuItemColor = {
+    applied: "#78B7D0",
+    waiting: "#FABC3F",
+    inteviewing: "#FFAD60",
+    offer: "#9CDBA6",
+    declined: "#FF6969"
+  }
+
+  const handleChange = (e: SelectChangeEvent) => {
+    setValue(e.target.value as string)
+  }
+
+  return (
+    <FormControl size="small" sx={{ verticalAlign: 'middle' }}>
+    <Select
+      value={value}
+      sx={{ backgroundColor: menuItemColor[value],borderRadius: 8, fontSize: "1rem", width: 150, lineHeight: 1 }}
+      onChange={handleChange}
+    >
+      <MenuItem value="applied">Applied</MenuItem>
+      <MenuItem value="waiting">Waiting</MenuItem>
+      <MenuItem value="inteviewing">Inteviewing</MenuItem>
+      <MenuItem value="offer">Offer</MenuItem>
+      <MenuItem value="declined">Declined</MenuItem>
+    </Select>
+  </FormControl>
+  )
+}
 
 const rows: GridRowsProp = [
   {
@@ -69,7 +118,7 @@ const rows: GridRowsProp = [
     company: "Google",
     source: "LinkedIn",
     resume: "SDEResumeV1.pdf",
-    status: "drop down menu",
+    status: "applied",
   },
   {
     id: 2,
@@ -79,7 +128,7 @@ const rows: GridRowsProp = [
     company: "Google",
     source: "LinkedIn",
     resume: "SDEResumeV1.pdf",
-    status: "drop down menu",
+    status: "waiting",
   },
   {
     id: 3,
@@ -89,7 +138,7 @@ const rows: GridRowsProp = [
     company: "Google",
     source: "LinkedIn",
     resume: "SDEResumeV1.pdf",
-    status: "drop down menu",
+    status: "inteviewing",
   },
   {
     id: 4,
@@ -99,13 +148,23 @@ const rows: GridRowsProp = [
     company: "Google",
     source: "LinkedIn",
     resume: "SDEResumeV1.pdf",
-    status: "drop down menu",
+    status: "declined",
+  },
+  {
+    id: 5,
+    date: "2024-10-26",
+    appNumber: "90",
+    jobRole: "Software Engineer",
+    company: "Google",
+    source: "LinkedIn",
+    resume: "SDEResumeV1.pdf",
+    status: "offer",
   },
 ]
 
 export default function ApplicationsTable() {
   return (
-    <Box py={2} display={"flex"} height="100%">
+    <Box py={2} display="flex">
       <DataGrid
         rows={rows}
         columns={columns}
