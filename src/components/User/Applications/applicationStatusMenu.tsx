a490a3dc35f92cc59ea804b8cef9568b07569226
Danielxburns <danielburnstech@gmail.com>
2024-11-01 10:03:56 -0500

    move StatusMenu into new file

---
+++ src/components/User/Applications/applicationStatusMenu.tsx
@@ -0,0 +1,52 @@
+import type { SelectChangeEvent} from "@mui/material";
+import { FormControl, Select, MenuItem } from "@mui/material"
+import type { GridRenderCellParams } from "@mui/x-data-grid"
+import { useState } from "react"
+
+export default function ApplicationStatusMenu(
+  params: GridRenderCellParams<String>,
+) {
+  const [value, setValue] = useState(params.value)
+
+  type MenuItemColor = {
+    applied: string
+    waiting: string
+    inteviewing: string
+    offer: string
+    declined: string
+  }
+
+  const menuItemColor: MenuItemColor = {
+    applied: "#78B7D0",
+    waiting: "#FABC3F",
+    inteviewing: "#FFAD60",
+    offer: "#9CDBA6",
+    declined: "#FF6969",
+  }
+
+  const handleChange = (e: SelectChangeEvent) => {
+    setValue(e.target.value as string)
+  }
+
+  return (
+    <FormControl size="small" sx={{ verticalAlign: "middle" }}>
+      <Select
+        value={value}
+        sx={{
+          backgroundColor: menuItemColor[value as keyof MenuItemColor],
+          borderRadius: 8,
+          fontSize: "1rem",
+          width: 150,
+          lineHeight: 1,
+        }}
+        onChange={handleChange}
+      >
+        <MenuItem value="applied">Applied</MenuItem>
+        <MenuItem value="waiting">Waiting</MenuItem>
+        <MenuItem value="inteviewing">Inteviewing</MenuItem>
+        <MenuItem value="offer">Offer</MenuItem>
+        <MenuItem value="declined">Declined</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
--- src/components/User/Applications/applicationsTable.tsx
+++ src/components/User/Applications/applicationsTable.tsx
@@ -1,13 +1,7 @@
-import type { SelectChangeEvent } from "@mui/material";
-import { Box, FormControl, MenuItem, Select } from "@mui/material";
-import type {
-  GridColDef,
-  GridRenderCellParams,
-  GridRowsProp,
-} from "@mui/x-data-grid";
-import { DataGrid } from "@mui/x-data-grid";
-import { useState } from "react";
-
+import { Box } from "@mui/material"
+import type { GridColDef, GridRowsProp } from "@mui/x-data-grid"
+import { DataGrid } from "@mui/x-data-grid"
+import ApplicationStatusMenu from "./applicationStatusMenu"
 
 const columns: GridColDef[] = [
   {
@@ -64,50 +58,9 @@ const columns: GridColDef[] = [
     headerAlign: "center",
     headerClassName: "text-white font-semi-bold text-sm",
     align: "center",
-    renderCell: StatusMenu
-  }
+    renderCell: ApplicationStatusMenu,
+  },
 ]
-
-function StatusMenu(params: GridRenderCellParams<String>) {
-
-  const [value, setValue] = useState(params.value)
-
-type MenuItemColor = {
-  applied: string;
-  waiting: string;
-  inteviewing: string;
-  offer: string;
-  declined: string;
-}
-
-  const menuItemColor: MenuItemColor = {
-    applied: "#78B7D0",
-    waiting: "#FABC3F",
-    inteviewing: "#FFAD60",
-    offer: "#9CDBA6",
-    declined: "#FF6969"
-  }
-
-  const handleChange = (e: SelectChangeEvent) => {
-    setValue(e.target.value as string)
-  }
-
-  return (
-    <FormControl size="small" sx={{ verticalAlign: 'middle' }}>
-    <Select
-      value={value}
-      sx={{ backgroundColor: menuItemColor[value],borderRadius: 8, fontSize: "1rem", width: 150, lineHeight: 1 }}
-      onChange={handleChange}
-    >
-      <MenuItem value="applied">Applied</MenuItem>
-      <MenuItem value="waiting">Waiting</MenuItem>
-      <MenuItem value="inteviewing">Inteviewing</MenuItem>
-      <MenuItem value="offer">Offer</MenuItem>
-      <MenuItem value="declined">Declined</MenuItem>
-    </Select>
-  </FormControl>
-  )
-}
 
 const rows: GridRowsProp = [
   {
